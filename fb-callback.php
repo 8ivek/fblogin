6607926615fd6ceb3b1527e3141650bf3161fa83
Bivek <bivek_j@yahoo.com>
2017-01-06 20:01:20 -0500

    added session, app id value changed

--- fb-callback.php
+++ fb-callback.php
@@ -13,30 +13,44 @@ $fb = new Facebook\Facebook([
 $helper = $fb->getRedirectLoginHelper();
 
 try {
-$accessToken = $helper->getAccessToken();
+    $accessToken = $helper->getAccessToken();
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
-// When Graph returns an error
-echo 'Graph returned an error: ' . $e->getMessage();
-exit;
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
-// When validation fails or other local issues
-echo 'Facebook SDK returned an error: ' . $e->getMessage();
-exit;
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
 }
 
-if (! isset($accessToken)) {
-if ($helper->getError()) {
-header('HTTP/1.0 401 Unauthorized');
-echo "Error: " . $helper->getError() . "\n";
-echo "Error Code: " . $helper->getErrorCode() . "\n";
-echo "Error Reason: " . $helper->getErrorReason() . "\n";
-echo "Error Description: " . $helper->getErrorDescription() . "\n";
-} else {
-header('HTTP/1.0 400 Bad Request');
-echo 'Bad request';
+if(!isset($accessToken)){
+    if($helper->getError()){
+        header('HTTP/1.0 401 Unauthorized');
+        echo "Error: " . $helper->getError() . "\n";
+        echo "Error Code: " . $helper->getErrorCode() . "\n";
+        echo "Error Reason: " . $helper->getErrorReason() . "\n";
+        echo "Error Description: " . $helper->getErrorDescription() . "\n";
+    }else{
+        header('HTTP/1.0 400 Bad Request');
+        echo 'Bad request';
+    }
+    exit;
 }
-exit;
+
+try {
+    // Returns a `Facebook\FacebookResponse` object
+    $response = $fb->get('/me?fields=id,name', $accessToken);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
 }
+$user = $response->getGraphUser();
+
+print_r($user);
 
 // Logged in
 echo '<h3>Access Token</h3>';
@@ -51,7 +65,7 @@ echo '<h3>Metadata</h3>';
 var_dump($tokenMetadata);
 
 // Validation (these will throw FacebookSDKException's when they fail)
-$tokenMetadata->validateAppId(201789086951728); // Replace {app-id} with your app id
+$tokenMetadata->validateAppId('201789086951728'); // Replace {app-id} with your app id
 // If you know the user ID this access token belongs to, you can validate it here
 //$tokenMetadata->validateUserId('123');
 $tokenMetadata->validateExpiration();
